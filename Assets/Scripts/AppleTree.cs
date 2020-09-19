89cbd81d4dd1a36b38ded068393ab07f23a365bb
shikamaru125 <70780812+shikamaru125@users.noreply.github.com>
2020-09-19 12:07:07 -0400

    Prefabs and Materials Created

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    //prefab for instantiating apples
+    public GameObject applePrefab;
+    //speed at which the appletree moves
+    public float speed = 1f;
+    //distance where appletree turns around
+    public float leftAndRightEdge = 10f;
+    //chance that the appletree will change directions
+    public float chanceToChangeDirections = 0.1f;
+    //rate at which apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //dropping apples every second
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        //changing direction
     }
 }
