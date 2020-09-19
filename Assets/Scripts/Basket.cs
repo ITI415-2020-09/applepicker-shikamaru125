0a434988103b59699802c17e9a18d8f092a093f0
shikamaru125 <70780812+shikamaru125@users.noreply.github.com>
2020-09-19 13:37:16 -0400

    Baskets Created/ Tied to Mouse

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -13,6 +13,15 @@ public class Basket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //get the current screen position of the mouse from input
+        Vector3 mousePos2D = Input.mousePosition;
+        //the camer'as z position sets how far to push the mouse into 3d
+        mousePos2D.z = -Camera.main.transform.position.z;
+        //convert the point from 2d screen space into 3d game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        //move the x position of this basket to the x position of the mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
     }
 }
